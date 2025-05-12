26d043028065319dc850a35c327de8d5fafe8d9d
Fractal-Tess <vgfractal@gmail.com>
2025-05-12 04:37:08 +0300

    feat: Enhance OpenAPI configuration
    Add OpenAPIDefinition to OpenApiConfig for improved API documentation, including title, version, and production server details.

--- server/src/main/java/com/explorer/explorer_api/config/OpenApiConfig.java
+++ server/src/main/java/com/explorer/explorer_api/config/OpenApiConfig.java
@@ -3,6 +3,9 @@ package com.explorer.explorer_api.config;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
 @SecurityScheme(
@@ -11,6 +14,12 @@ import org.springframework.context.annotation.Configuration;
     scheme = "bearer",
     bearerFormat = "JWT",
     description = "JWT Authorization header using the Bearer scheme. Example: 'Authorization: Bearer {token}'"
+)
+@OpenAPIDefinition(
+    info = @Info(title = "Explorer API", version = "v1"),
+    servers = {
+        @Server(url = "https://explorer.fractal-tess.xyz", description = "Production Server")
+    }
 )
 public class OpenApiConfig {
 } 
